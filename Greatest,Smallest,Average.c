ab9b319123f1f4e769e54fc6e7557c7b20c84ae4
Taimoor Masaud <55857068+TaimoorMasaud@users.noreply.github.com>
2019-10-14 22:18:44 +0500

    Add files via upload

---
+++ Greatest,Smallest,Average.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char ch='Y';
+    int number,largest,smallest,average=0,a=1;
+    printf("\nEnter a number :");
+    scanf("%d",&number);
+    largest=number,smallest=number;
+     while (ch=='y'||ch=='Y')
+    {
+        printf("\nEnter another number :");
+        scanf("%d",&number);
+        if (number>largest)
+        {
+            largest=number;
+        }
+        else if (number<smallest)
+        {
+            smallest=number;
+        }
+        average=average+number;
+        a++;
+
+        printf("\nDo you want to enter another value Y/N? ");
+        scanf(" %c",&ch);
+
+    }
+    average=(average+number)/a;
+
+    printf("\nSmallest=%d \nLargest=%d \nAverage=%d",smallest,largest,average);
+}
